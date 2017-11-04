9b70f313ccf63041a85a3225b545a2538e35f837
pluboj <pluboj@yahoo.com>
2017-11-03 23:51:17 -0700

    Form setup

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,8 +12,16 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Text Extractor v1.0");
+
+        //Grid Pane
+        GridPane gridPane = new GridPane();
+
+
+        Scene scene = new Scene(gridPane, 500, 350);
+        scene.getStylesheets().add("/resources/css/main.css");
+        primaryStage.setScene(scene);
+
         primaryStage.show();
     }
 
