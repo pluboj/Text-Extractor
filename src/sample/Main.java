58ff74697d3f9f9ceea48d6bf472e68a9796a935
pluboj <pluboj@yahoo.com>
2017-11-04 23:49:46 -0700

    Add controls

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,7 +6,10 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -17,10 +20,15 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Text Extractor v1.0");
 
+
+        /*ChoiceBox*/
+        ChoiceBox<String> documents = new ChoiceBox<>();
+        documents.getItems().addAll("Word", "Excel");
+
         /*Text input*/
         TextField textUrl = new TextField();
         textUrl.setPromptText("Paste Url");
-        textUrl.setPrefWidth(600);
+        textUrl.setPrefWidth(500);
 
         /*Grid Pane*/
         GridPane gridPane = new GridPane();
@@ -29,8 +37,18 @@ public class Main extends Application {
         gridPane.setVgap(10);
         gridPane.setPadding(new Insets(25, 25, 25, 25));
 
+        /*Labels*/
+        Label lblDocuments = new Label("Select type of document: ");
+        lblDocuments.setLabelFor(documents);
+
+        Label lblTextUrl = new Label("Paste URL: ");
+        lblTextUrl.setLabelFor(textUrl);
+
         /*Add elements to gridPane*/
-        gridPane.add(textUrl,0,1);
+        gridPane.addRow(0,lblDocuments);
+        gridPane.addRow(1, documents);
+        gridPane.addRow(2, lblTextUrl);
+        gridPane.addRow(3, textUrl);
 
         Scene scene = new Scene(gridPane, 500, 350);
         scene.getStylesheets().add("/resources/css/main.css");
