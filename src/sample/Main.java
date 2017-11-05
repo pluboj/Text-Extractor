9fa79fd6f3f72a32ea40764a1d415f8be2a1267d
pluboj <pluboj@yahoo.com>
2017-11-05 00:33:08 -0700

    Add controls

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,15 +6,14 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    ChoiceBox<String> documents;
+    ProgressIndicator indeterminateInd;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -22,8 +21,11 @@ public class Main extends Application {
         primaryStage.setTitle("Text Extractor v1.0");
 
         /*ChoiceBox*/
-        ChoiceBox<String> documents = new ChoiceBox<>();
+        documents = new ChoiceBox<>();
         documents.getItems().addAll("Word", "Excel");
+
+        /*ProgressBar*/
+        indeterminateInd = new ProgressIndicator(0);
 
         /*Text input*/
         TextField textUrl = new TextField();
@@ -55,19 +57,22 @@ public class Main extends Application {
         gridPane.addRow(2, lblTextUrl);
         gridPane.addRow(3, textUrl);
         gridPane.addRow(6, btnProcess);
+        gridPane.addRow(7, indeterminateInd);
 
         Scene scene = new Scene(gridPane, 500, 350);
         scene.getStylesheets().add("/resources/css/main.css");
         primaryStage.setScene(scene);
+
+
 
         primaryStage.show();
     }
 
     //TODO:pl - process doc
     private void processDocument() {
-        System.out.println("button was clicked");
+        System.out.println(documents.getValue());
+        indeterminateInd.setProgress(1.0);
     }
-
 
     public static void main(String[] args) {
         launch(args);
